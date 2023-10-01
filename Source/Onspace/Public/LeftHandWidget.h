a049c6502d4f6f0e2ceec22b7b0cb70f901cb999
imagine99 <57169754+imagine99@users.noreply.github.com>
2023-10-02 02:07:07 +0900

    Commit55
    Create Widget class
    BP work finished and than transfer C++ code...

---
+++ Source/Onspace/Public/LeftHandWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LeftHandWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONSPACE_API ULeftHandWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Onspace/Public/RightHandWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RightHandWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONSPACE_API URightHandWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
